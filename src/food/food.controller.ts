cd2fa9032ff6970fa90537f432a702ce4b106a3b
balhyo-younjisang <yjs12180825@gmail.com>
2024-08-11 11:38:12 +0900

    c

--- src/food/food.controller.ts
+++ src/food/food.controller.ts
@@ -9,7 +9,7 @@ export class FoodController {
 
     constructor(private readonly foodService: FoodService, private readonly geminiService: GeminiService) { }
 
-    @Get("search")
+    @Get("search/:query")
     @ApiOperation({ summary: 'get food database' })
     @ApiResponse({ status: 200 })
     async searchFood(@Req() req, @Param('query') query: string) {
